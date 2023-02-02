8531fb5c66be2819cc63e4888929acbd07ac70a3
Yoshi-program <Yoshi-program@users.noreply.github.com>
2023-02-02 16:27:31 +0000

    deploy: 432c15a802de3845dd2ce97988eee741e2a55fcd

---
+++ _next/static/lldrEXIiTiMMI-L3_7EbC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/703-618baac3892ec6e0.js","static/chunks/pages/index-942913ecdf8a72df.js"],"/_error":["static/chunks/pages/_error-29b3d7bb596dbd7b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
