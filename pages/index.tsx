05afe2622797648aa8d0c56e013909524b6b724f
s1F102103303 <s1F102103303@iniad.org>
2023-02-02 11:20:57 +0900

    QRコードのcss調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,39 @@ import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import qrcode from 'qrcode'
 import { useRouter } from 'next/router'
+import styled from 'styled-components'
+
+const QrCodeImg = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 80%;
+  max-width: 100%;
+  height: auto;
+  transform: translate(-50%, -50%);
+`
 
 const Home: NextPage = () => {
-  const [tmp, setTmp] = useState<string>('')
+  const [qrCodeData, setQrCodeData] = useState<string>('')
 
   const currentVersion = '1.0.0'
   const currentAppId = '1'
   const router = useRouter()
   const token = router.query.token
 
-  const qrText: string = JSON.stringify({
+  const qrCodeText: string = JSON.stringify({
     version: currentVersion,
     appId: currentAppId,
     token: token,
   })
 
   useEffect(() => {
-    qrcode.toDataURL(qrText, function (err, url) {
-      setTmp(url)
+    qrcode.toDataURL(qrCodeText, (err, url) => {
+      setQrCodeData(url)
     })
-  }, [qrText])
+  }, [qrCodeText])
 
-  return <img src={tmp} />
+  return <QrCodeImg src={qrCodeData} alt="QRCode-image" />
 }
 
 export default Home
