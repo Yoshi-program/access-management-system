be2ee522ab7f68375d40d55c51938897f9a97250
s1F102103303 <s1F102103303@iniad.org>
2023-02-07 13:38:52 +0900

    ユーザー登録を行う場所を別に作った

--- server/service/index.ts
+++ server/service/index.ts
@@ -1,76 +1,86 @@
 import fastify from 'fastify'
-import type { GuildMember, TextChannel, Message } from 'discord.js'
-import { Client } from 'discord.js'
-import dotenv from 'dotenv'
-import crypto from 'crypto'
+// import type { GuildMember, TextChannel, Message } from 'discord.js'
+// import { Client } from 'discord.js'
+// import dotenv from 'dotenv'
+// import crypto from 'crypto'
 import type { IBodystring } from '../types/access'
-import type registerContent from '../types/register'
+// import type registerContent from '../types/register'
 import checkAccess from './access'
 // import handler from './discord'
 import { PrismaClient } from '@prisma/client'
 
-dotenv.config()
+// dotenv.config()
 
 const prisma = new PrismaClient()
 
-const qrCodeText: string = JSON.stringify({
-  version: '1.0.0',
-  appId: '1',
-  token: 'snfjvkdcsifa',
-})
+// const qrCodeText: string = JSON.stringify({
+//   version: '1.0.0',
+//   appId: '1',
+//   token: 'snfjvkdcsifa',
+// })
 
-const register = async (content: registerContent) => {
-  const createdToken = crypto.randomUUID()
-  const user = await prisma.user.create({
-    data: {
-      name: content.name,
-      userId: crypto.randomUUID(),
-      studentId: content.studentId,
-      discordId: content.discordId,
-      token: createdToken,
-      floor: content.floor,
-    },
-  })
-  console.log(user)
-  return createdToken
-}
+// const register = async (content: registerContent) => {
+//   const createdToken = crypto.randomUUID()
+//   const user = await prisma.user.create({
+//     data: {
+//       name: content.name,
+//       userId: crypto.randomUUID(),
+//       studentId: content.studentId,
+//       discordId: content.discordId,
+//       token: createdToken,
+//       floor: content.floor,
+//     },
+//   })
+//   console.log(user)
+//   return createdToken
+// }
 
-const statusCode = 200
-const client = new Client({
-  intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
-})
+// const statusCode = 200
+// const client = new Client({
+//   intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
+// })
 
-client.once('ready', () => {
-  console.log('Ready!')
-  console.log(client.user?.tag)
-})
+// client.once('ready', () => {
+//   console.log('Ready!')
+//   console.log(client.user?.tag)
+// })
 
-client.on('messageCreate', async (message: Message) => {
-  if (message.author.bot) return
-  console.log(message.author)
-  if (message.content.includes('name')) {
-    const content: registerContent = JSON.parse(message.content)
-    content.discordId = message.author.id
-    content.name = message.author.username //  + message.author.discriminator
-    await register(content).then((token) => {
-      console.log('登録できた')
-      const user = client.users.cache.get(`${message.author.id}`)
-      if (user) {
-        const sendText = `https://localhost:3000/?token=${token}` // PWAの導入に関する文章（参考URLを入れて）も加える
-        user.send(sendText)
-      }
-    })
-  }
-})
-// '{"studentId": "111112", "floor": "24"}'
+// client.on('messageCreate', async (message: Message) => {
+//   if (message.author.bot) return
+//   console.log(message.author)
+//   if (message.content.includes('name')) {
+//     const content: registerContent = JSON.parse(message.content)
+//     content.discordId = message.author.id
+//     content.name = message.author.username //  + message.author.discriminator
+//     await register(content).then((token) => {
+//       console.log('登録できた')
+//       const user = client.users.cache.get(`${message.author.id}`)
+//       if (user) {
+//         const sendText = `https://localhost:3000/?token=${token}` // PWAの導入に関する文章（参考URLを入れて）も加える
+//         user.send(sendText)
+//       }
+//     })
+//   }
+// })
+// "{'studentId': '111112', 'floor': '24'}"
+// '{"studentId": "1f1021000000", "floor": "2"}'
 
-client.login(process.env.DiscordBotTOKEN)
+// client.login(process.env.DiscordBotTOKEN)
 
 const server = fastify()
 
-server.post<{ Body: IBodystring }>('/access', async (req, reply) => {
+server.post<{ Body: string }>('/access', async (req, reply) => {
   console.log('request.body: ', req.body)
-  checkAccess(req.body)
+  const accessData: IBodystring = JSON.parse(req.body)
+  checkAccess(accessData)
+  return req.body
+})
+
+server.post<{ Body: string }>('/register', async (req, reply) => {
+  console.log('request.body: ', req.body)
+  const a = JSON.parse(req.body)
+  const user = await prisma.user.create(a)
+  console.log(user)
   return req.body
 })
 
