b6cd8e92cb48d92098a43d5eea33c2e2a99e82df
s1F102103303 <s1F102103303@iniad.org>
2023-02-13 12:01:51 +0900

    api routesができるかテスト

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,49 +1,50 @@
-import type { GuildMember, TextChannel } from 'discord.js'
-import { Client } from 'discord.js'
-import dotenv from 'dotenv'
+// import type { GuildMember, TextChannel } from 'discord.js'
+// import { Client } from 'discord.js'
+// import dotenv from 'dotenv'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-dotenv.config()
+// dotenv.config()
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const statusCode = 200
-  const client = new Client({
-    intents: ['Guilds', 'GuildMembers', 'GuildMessages'],
-  })
+  res.status(statusCode).json({ name: 'John Doe' })
+  // const client = new Client({
+  //   intents: ['Guilds', 'GuildMembers', 'GuildMessages'],
+  // })
 
-  client.once('ready', () => {
-    console.log('Ready!')
-    console.log(client.user?.tag)
-  })
+  // client.once('ready', () => {
+  //   console.log('Ready!')
+  //   console.log(client.user?.tag)
+  // })
 
-  client.on('ready', async () => {
-    if (req.body.qrcode != '') {
-      if (process.env.CHANNEL_ID) {
-        const channel = (await client.channels.cache.get(process.env.CHANNEL_ID)) as TextChannel
-        channel.send('in')
-      }
-    }
-  })
+  // client.on('ready', async () => {
+  //   if (req.body.qrcode != '') {
+  //     if (process.env.CHANNEL_ID) {
+  //       const channel = (await client.channels.cache.get(process.env.CHANNEL_ID)) as TextChannel
+  //       channel.send('in')
+  //     }
+  //   }
+  // })
 
-  client.on('guildMemberAdd', (member: GuildMember) => {
-    // memberのIDを取得
-    // QRコードの作成
-    // 個人QRコードが載っているサイトへのURLを個人DMに送信
-    // PWAの説明nのためのサイトのURLを個人DMに送信
-  })
+  // client.on('guildMemberAdd', (member: GuildMember) => {
+  //   // memberのIDを取得
+  //   // QRコードの作成
+  //   // 個人QRコードが載っているサイトへのURLを個人DMに送信
+  //   // PWAの説明nのためのサイトのURLを個人DMに送信
+  // })
 
-  client.login(process.env.TOKEN)
+  // client.login(process.env.TOKEN)
 
-  switch (req.method) {
-    case 'POST': {
-      res.status(statusCode).json({ qrcode: req.body.qrcode })
-      break
-    }
-    default: {
-      res.status(statusCode).json({ name: 'John Doe' })
-      break
-    }
-  }
+  // switch (req.method) {
+  //   case 'POST': {
+  //     res.status(statusCode).json({ qrcode: req.body.qrcode })
+  //     break
+  //   }
+  //   default: {
+  //     res.status(statusCode).json({ name: 'John Doe' })
+  //     break
+  //   }
+  // }
 }
 
 export default handler
